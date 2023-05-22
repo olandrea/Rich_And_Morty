95fc8fa8172ea706f8a01e74d471bd43d2b79aef
olandrea <andreaochoaispuru@gmail.com>
2023-05-22 16:54:59 -0300

    Server commit 2

--- Server/src/index.js
+++ Server/src/index.js
@@ -5,9 +5,9 @@ http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     if(req.url.includes("/rickandmorty/character")) {
-        //console.log(req.url.split('/').at(-1))
+       // console.log(req.url.split('/').at(-1))
         const id = req.url.split('/').at(-1);
 
         getCharById(res, Number(id));
     }
-}).listen(3001);
+}).listen(3001, "localhost");
