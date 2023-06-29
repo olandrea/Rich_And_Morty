40b64f63e46d06e06ea69f14e48ed440fa372fe3
olandrea <andreaochoaispuru@gmail.com>
2023-06-29 19:55:27 -0300

    server..

--- Server/src/Controllers/getCharById.js
+++ Server/src/Controllers/getCharById.js
@@ -3,6 +3,7 @@ const URL = "https://rickandmortyapi.com/api/character"
 
 const getCharById = async (req, res) => {
   try {
+
     const {id} = req.params;
     const {name, gender, species, origin, image, status} = await axios.get(`${URL}/${id}`).data;
     
@@ -15,8 +16,10 @@ const getCharById = async (req, res) => {
       status,
       species
     };
+    
     return character.name ? res.json(character) : res.status(404).send('Not Found!')
   } catch (error) {
+
     res.status(500).send(error.message);
   }    
 } ;
